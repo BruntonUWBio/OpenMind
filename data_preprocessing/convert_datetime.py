83369ec9df1f208c47e7d925f715a3af68ccb37a
valentina-s <vms16@uw.edu>
2017-12-08 00:58:30 -0800

    renaming file

--- data_preprocessing/convert_datetime.py
+++ data_preprocessing/convert_datetime.py
@@ -1,19 +1,19 @@
-"""
-align_vid_ecog.py converts vid_real_time files to a more appropriate format for indexing
+import pandas as pd
+import sys
+import argparse
+from argparse import RawTextHelpFormatter
+
+# parser = argparse.ArgumentParser(description="")
+
+"""convert_datetime.py converts vid_real_time files to a more appropriate format for indexing
 final table is of this form
 ------------------------------
 |patient|session|vid|datetime|
 |  ...  |  ...  |...|  ...   |
-
-
-Usage:  python align_vid_ecog.py /data1/sharedata/vid_real_time/a86a4375_2 output.csv
-
-
-"""
-
-
-import pandas as pd
-import sys
+Usage:  python convert_datetime.py /data1/sharedata/vid_real_time/a86a4375_2 output.csv"""
+#formatter_class=RawTextHelpFormatter)
+#parser = argparse.ArgumentParser()
+#parser.add_argument(help="help")
 
 # reading the file for this session
 session_name = sys.argv[1]
