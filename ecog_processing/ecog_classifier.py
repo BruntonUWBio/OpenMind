edd1cb45c74d36196c5f8286d936570ea7de9beb
gvelchuru <gvelchuru@gmail.com>
2018-04-28 01:00:48 -0700

    ready for cylon

--- ecog_processing/ecog_classifier.py
+++ ecog_processing/ecog_classifier.py
@@ -5,35 +5,77 @@ from sklearn.decomposition import PCA
 import seaborn as sns
 import argparse
 import h5py
+from tqdm import tqdm
+from tpot import TPOTClassifier
+from sklearn.model_selection import train_test_split
+
+
+def run_tpot(zeros, ones):
+    y = [0 for x in zeros]
+    y.extend([1 for x in ones])
+    all_data = da.concatenate([zeros, ones]).compute()
+    X_train, X_test, y_train, y_test = train_test_split(
+        all_data, y, test_size=.33)
+    pca = PCA(n_components=4)
+    X_train = pca.fit_transform(X_train)
+    X_test = pca.fit_transform(X_test)
+
+    if not os.path.exists('tpot_checkpoint'):
+        os.mkdir('tpot_checkpoint')
+
+    if not os.path.exists('tpot_cache'):
+        os.mkdir('tpot_cache')
+    tpot = TPOTClassifier(
+        n_jobs=-1,
+        verbosity=3,
+        scoring='f1',
+        subsample=.5,
+        periodic_checkpoint_folder='tpot_checkpoint',
+        max_eval_time_mins=20,
+        memory='tpot_cache')
+    tpot.fit(X_train, y_train)
+    print(tpot.score(X_test, y_test))
+    tpot.export('tpot_ecog_pipeline.py')
 
 
 def elbow_curve(data):
+    data = data[1]
     components = range(1, data.shape[1] + 1)
     explained_variance = []
 
-    for component in components:
-        explained_variance.append(PCA(component).explained_variance_ratio)
+    for component in tqdm(components[:50]):
+        pca = PCA(n_components=component)
+        pca.fit(data)
+        explained_variance.append(sum(pca.explained_variance_ratio_))
     sns_plot = sns.regplot(
-        x=components, y=explained_variance, fit_reg=False).get_figure()
+        x=components[:50], y=explained_variance, fit_reg=False).get_figure()
     sns_plot.savefig("pca_elbow.png")
 
 
-def get_data(data_loc: str):
-    data_folders = os.listdir(data_loc)
+def get_data(data_loc: str) -> tuple:
+    data_folders = [os.path.join(data_loc, x) for x in os.listdir(data_loc)]
     out_zeros = None
     out_ones = None
 
-    # for data_file in data_files:
-    # x = h5py.File(data_file)
-
-    # if '/0' in x:
-    # print(x['/0'])
-
-    for data_folder in data_folders:
+    for data_folder in tqdm(data_folders):
         zero_folder = os.path.join(data_folder, '0')
-        one_folder = os.path.join(one_folder, '1')
+        one_folder = os.path.join(data_folder, '1')
 
         if os.path.exists(zero_folder):
+            if out_zeros is None:
+                out_zeros = da.from_npy_stack(zero_folder)
+            else:
+                out_zeros = da.concatenate(
+                    [out_zeros, da.from_npy_stack(zero_folder)])
+
+        if os.path.exists(one_folder):
+            if out_ones is None:
+                out_ones = da.from_npy_stack(one_folder)
+            else:
+                out_ones = da.concatenate(
+                    [out_ones, da.from_npy_stack(one_folder)])
+
+    return out_zeros.compute(), out_ones.compute()
 
 
 if __name__ == '__main__':
@@ -41,5 +83,6 @@ if __name__ == '__main__':
     parser.add_argument('-d', required=True, help="Path to data")
     args = vars(parser.parse_args())
     DATA_LOC = args['d']
-    get_data(DATA_LOC)
+    zeros, ones = get_data(DATA_LOC)
+    run_tpot(zeros, ones)
     # elbow_curve(get_data(DATA_LOC))
--- ecog_processing/ecog_emotion.py
+++ ecog_processing/ecog_emotion.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import argparse
+from collections import deque
 import json
 import random
 import sys
@@ -14,6 +15,7 @@ import os
 import numpy as np
 import mne
 from glob import glob
+from chest import Chest
 from mne.io import read_raw_edf
 from welch import get_events
 from pathos.multiprocessing import ProcessingPool as Pool
@@ -22,6 +24,8 @@ from tqdm import tqdm
 from typing import List, Dict
 from scipy.signal import welch
 from contextlib import contextmanager
+# from memory_profiler import profile
+from dask.diagnostics import ProgressBar
 
 
 @contextmanager
@@ -35,11 +39,13 @@ def suppress_stdout():
             sys.stdout = old_stdout
 
 
-def get_window_data(raw: mne.io.Raw, times: list, picks,
-                    eventTimes: set) -> tuple:
+def get_window_data(raw: mne.io.Raw, times, picks, eventTimes, tqdm_num,
+                    filename) -> tuple:
     ECOG_SAMPLING_FREQUENCY = 1000  # ECoG samples at a rate of 1000 Hz
     EVENT_DELTA_SECONDS = 1
     all_times = len(raw)
+    times = deque(sorted(times))
+    eventTimes = deque(sorted(list(eventTimes)))
     ecog_start_time = sorted(times)[0]
     one_data = None
     zero_data = None
@@ -48,8 +54,11 @@ def get_window_data(raw: mne.io.Raw, times: list, picks,
         range_times, int(len(range_times) / (ECOG_SAMPLING_FREQUENCY)))
     freqs = None
 
-    for split_time in split_range_times:
-        gc.collect()
+    for index, split_time in enumerate(
+            tqdm(split_range_times, position=tqdm_num, desc=filename)):
+
+        if not times:
+            break
         ecog_time_arr_start = split_time[0]
         ecog_time_arr_end = split_time[len(split_time) - 1]
         real_pos = ecog_start_time + \
@@ -61,29 +70,49 @@ def get_window_data(raw: mne.io.Raw, times: list, picks,
         has_event = False
         has_been_annotated = False
 
-        for time in times:
-            if real_pos <= time <= emote_window_end:
+        while times:
+            time = times[0]
+
+            if real_pos <= time < emote_window_end:
                 has_been_annotated = True
 
+            if time >= emote_window_end:
                 break
+            times.popleft()
 
-            if time > emote_window_end:
-                break
+        # for time in times:
+        # if real_pos <= time <= emote_window_end:
+        # has_been_annotated = True
+
+        # break
+
+        # if time > emote_window_end:
+        # break
 
         if not has_been_annotated:
             continue
 
-        for event_time in eventTimes:
-            if real_pos <= event_time <= emote_window_end:
+        while eventTimes:
+            eventTime = eventTimes[0]
+
+            if real_pos <= eventTime < emote_window_end:
                 has_event = True
 
+            if eventTime >= emote_window_end:
                 break
+            eventTimes.popleft()
 
-            if event_time > emote_window_end:
-                break
+        # for event_time in eventTimes:
+        # if real_pos <= event_time < emote_window_end:
+        # has_event = True
+
+        # break
+
+        # if event_time > emote_window_end:
+        # break
         # try:
         data, ecog_times = raw[picks, ecog_time_arr_start:ecog_time_arr_end]
-        data = da.from_array(data, chunks=(100, -1))
+        data = da.from_array(data, chunks=(1000, -1))
         psd = welch(data)
         # print("DIFFERENCE BETWEEN ECOG TIMES IS: {0}".format(
         # ecog_time_arr_end - ecog_time_arr_start))
@@ -91,30 +120,40 @@ def get_window_data(raw: mne.io.Raw, times: list, picks,
         if freqs is None:
             freqs = psd[0]
 
+        psd_data = psd[1]
+
+        if np.allclose(psd_data, np.zeros(psd_data.shape)):
+            continue
+
         if has_event:
             if one_data is None:
-                one_data = da.from_array(psd[1], chunks=(1000, -1))
+                one_data = da.from_array(psd[1], chunks=(10000, -1))
             else:
                 one_data = da.concatenate([one_data, psd[1]])
+                # da.rechunk(one_data, chunks=(10000, -1))
+                one_data = one_data.compute()
         else:
             if zero_data is None:
-                zero_data = da.from_array(psd[1], chunks=(1000, -1))
+                zero_data = da.from_array(psd[1], chunks=(10000, -1))
             else:
                 zero_data = da.concatenate([zero_data, psd[1]])
-
-    print("LEN ONES: {0}".format(one_data.shape))
-    print("LEN ZEROS: {0}".format(zero_data.shape))
+                # da.rechunk(zero_data, chunks=(10000, -1))
+                zero_data = zero_data.compute()
 
     return freqs, one_data, zero_data
 
 
 def find_filename_data(au_emote_dict_loc, classifier_loc, real_time_file_loc,
                        out_loc, out_q, filename):
+    tqdm_num, filename = filename
+    tqdm_num = (tqdm_num % 5) + 1
     au_emote_dict = json.load(open(au_emote_dict_loc))
     try:
         with suppress_stdout():
             raw = read_raw_edf(filename, preload=False)
     except ValueError:
+        print('{0} has errors'.format(filename))
+
         return
     mapping = {
         ch_name: 'ecog'
@@ -137,14 +176,14 @@ def find_filename_data(au_emote_dict_loc, classifier_loc, real_time_file_loc,
 
     if times is None:
         times = []
-    print("{0} number of times: {1}".format(filename, len(times)))
+    # print("{0} number of times: {1}".format(filename, len(times)))
 
     if times:
         predicDic = {time: predic for time, predic in zip(times, corr)}
         eventTimes = set(x[0] for x in events)
         picks = mne.pick_types(raw.info, ecog=True)
         freqs, temp_one_data, temp_zero_data = get_window_data(
-            raw, times, picks, eventTimes)
+            raw, times, picks, eventTimes, tqdm_num, filename)
 
         if freqs is not None:
             freqs = da.from_array(freqs, chunks=(100, ))
@@ -161,12 +200,14 @@ def find_filename_data(au_emote_dict_loc, classifier_loc, real_time_file_loc,
 
             if temp_zero_data is not None:
                 da.to_npy_stack(
-                    os.path.join(filename_out_dir, '0'), temp_zero_data)
+                    os.path.join(filename_out_dir, '0'),
+                    da.from_array(temp_zero_data, chunks=(10000, -1)))
 
             if temp_one_data is not None:
                 da.to_npy_stack(
-                    os.path.join(filename_out_dir, '1'), temp_one_data)
-    out_q.put(filename)
+                    os.path.join(filename_out_dir, '1'),
+                    da.from_array(temp_one_data, chunks=(10000, -1)))
+    out_q.put((filename, len(times)))
 
 
 def clean_filenames(filenames: list, au_dict: dict) -> list:
@@ -195,10 +236,17 @@ def listener(fn, q):
                 break
             f = open(fn, 'r')
             curr_arr = json.load(f)
-            curr_arr.append(m)
+
+            if m not in curr_arr:
+                curr_arr[m[0]] = m[1]
             f = open(fn, 'w')
             json.dump(curr_arr, f)
+            f.flush()
     f.close()
+
+
+def clean_base(fn):
+    return os.path.basename(fn).replace('.edf', '')
 
 
 if __name__ == '__main__':
@@ -224,29 +272,42 @@ if __name__ == '__main__':
     if os.path.exists(ALREADY_DONE_FILE):
         already_done_dict = json.load(open(ALREADY_DONE_FILE))
     else:
-        already_done_dict = []
+        already_done_dict = {}
         json.dump(already_done_dict, open(ALREADY_DONE_FILE, 'w'))
     m = multiprocessing.Manager()
     zero_data = m.list()  # type: List[List[float]]
     one_data = m.list()  # type: List[List[float]]
     filenames = glob(os.path.join(EDF_DIR, '**/*.edf'), recursive=True)
+    already_done_dirs = os.listdir(
+        os.path.join(OUT_FILE_PATH, 'classifier_data'))
     filenames = [
         x
         for x in clean_filenames(filenames, json.load(open(AU_EMOTE_DICT_LOC)))
-        if x not in already_done_dict
+        if (clean_base(x) not in already_done_dirs and (
+            x not in already_done_dict or (already_done_dict[x] > 0)))
     ]
     out_q = m.Queue()
+
+    Thread(target=listener, args=(ALREADY_DONE_FILE, out_q)).start()
+
+    # for filename in tqdm(filenames):
+    # find_filename_data(AU_EMOTE_DICT_LOC, CLASSIFIER_LOC,
+    # REAL_TIME_FILE_LOC, OUT_FILE_PATH, out_q, filename)
+
     f = functools.partial(find_filename_data, AU_EMOTE_DICT_LOC,
                           CLASSIFIER_LOC, REAL_TIME_FILE_LOC, OUT_FILE_PATH,
                           out_q)
+    NUM_PROCESSES = 5
     with tqdm(total=len(filenames)) as pbar:
-        p = Pool(4)
+        p = Pool(NUM_PROCESSES)
 
-    Thread(target=listener, args=(ALREADY_DONE_FILE, out_q)).start()
-    # watcher = p.apply_async(listener, (ALREADY_DONE_FILE, out_q))
+        for iteration, _ in enumerate(p.uimap(f, enumerate(filenames))):
+            pbar.update()
 
-    for iteration, _ in enumerate(p.uimap(f, filenames)):
-        pbar.update()
-
-    out_q.put('kill')
-    p.close()
+        # for filename in filenames:
+        # find_filename_data(AU_EMOTE_DICT_LOC, CLASSIFIER_LOC,
+        # REAL_TIME_FILE_LOC, OUT_FILE_PATH, out_q,
+        # (0, filename))
+        out_q.put('kill')
+        p.close()
+        p.join()
